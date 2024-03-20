fbcd7129e6a5dbae5e5230a138277aac1f8564da
hugo-esquer <hugo.esquer@laplateforme.io>
2024-03-20 14:28:13 +0100

    manipulation_texte et scripting_avancé

---
+++ manipulation_texte.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cat > script.py << EOF
+import csv
+data = [["Jean", "25 ans", "Paris"],
+["Marie", "30 ans", "Lyon"],
+["Pierre", "22 ans", "Marseille"],
+["Sophie", "35 ans", "Toulouse"]]
+
+with open("data.csv", "w") as f:
+	writer = csv.writer(f)
+	writer.writerows(data)
+EOF
+
+chmod +x script.py
+
+python3 script.py
---
+++ scripting_avancé.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+date=`date "+%Y%m%d"`
+SOURCE="/home/hugo/Documents/Plateforme"
+BACKUP="/home/hugo/Documents/backup"
+HISTORIC="/home/hugo/Documents/backup/$date"
+
+if [ ! -d "$SOURCE" ]; then
+	echo "Erreur le fichier Plateforme n'existe pas"
+	exit 1
+fi
+
+if [ ! -d "$BACKUP" ]; then
+	mkdir "$BACKUP"
+fi
+
+if [ ! -d "$HISTORIC" ]; then
+	mkdir "$HISTORIC"
+fi
+
+cp -R "$SOURCE"/* "$HISTORIC"
+
+if [ $? -eq 0 ]; then
+	echo "Sauvegarde effectuée avec succès."
+else
+	echo "Erreur lors de la sauvegarde."
+fi
