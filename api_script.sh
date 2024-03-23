85e6a26c2b80d6ee828f7665e74b5c8c3adbdb5c
hugo-esquer <esquer.hugo@laplateforme.io>
2024-03-23 22:06:11 +0100

    <fix>[api_script.sh]:<selection correcte du début et de la fin de la seimaine>

--- api_script.sh
+++ api_script.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
 
-#secret.cfg:
-#API_KEY="XXXXXXXXXXXXXXXXXXXXXXXX"
+source "/home/hugo/secret.cfg"
 
-source /home/hugo/secret.cfg
+url="https://api.football-data.org/v4/competitions/FL1/matches"
 
-URL="https://api.football-data.org/v2/competitions/FL1/matches"
+start_of_week=$(date -d "last Monday" "+%Y-%m-%d")
+end_of_week=$(date -d "next Sunday" "+%Y-%m-%d")
 
-start_of_week=$(date -d "last sunday" +%Y-%m-%d)
+response=$(curl -s "${url}?dateFrom=${start_of_week}&dateTo=${end_of_week}" \
+           -H "X-Auth-Token: ${API_KEY}")
 
-response=$(curl -s -H "X-Auth-Token: $API_KEY" "$URL")
-
-if [ $? -eq 0 ]; then
-    matches=$(echo "$response" | jq -r --arg start "$start_of_week" '.matches | map(select(.utcDate | startswith($start)))')
-    echo "Liste des matchs de la Ligue 1 de la semaine du $start_of_week :"
-    echo "---------------------------------------------------------------------------"
-    echo "$matches" | jq -r '.[] | "\(.homeTeam.name) vs \(.awayTeam.name) - \(.utcDate)"'
-else
-    echo "Erreur lors de la récupération des données. Veuillez vérifier votre clé API ou votre connexion Internet."
+if [[ ${response} =~ "<html>" ]]; then
+    echo "Erreur de récupération des données, veuillez vérifier votre connection internet ou votre clé API."
+    exit 1
 fi
 
+match_list=$(echo "${response}" | jq -r '.matches[] | .homeTeam.name, .awayTeam.name, .utcDate')
+
+echo "Liste des matchs de la Ligue 1 de la semaine du ${start_of_week} au ${end_of_week}:"
+echo "------------------------------------------------------------------------------"
+for entry in $(echo "${match_list}"); do
+    echo "${entry}"
+done
